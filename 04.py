52be3fb2337f5ae95aa72bc072bdaf6ac7356252
Kasia Kulma <katarzyna.kulma@gmail.com>
2022-12-06 11:32:37 +0000

    Day 4 - part 2

--- 04.py
+++ 04.py
@@ -7,14 +7,6 @@ input_list = input.split('\n')
 
 
 # ### PART 1
-# input_list = [
-#     '2-4,6-8',
-#     '2-3,4-5',
-#     '5-7,7-9',
-#     '2-8,3-7',
-#     '6-6,4-6',
-#     '2-6,4-8'
-# ]
 
 full_overlaps = 0
 for element in input_list:
@@ -27,6 +19,28 @@ for element in input_list:
         full_overlaps+=1
 
 
-
-
 ### PART 2
+# input_list = [
+#     '2-4,6-8',
+#     '2-3,4-5',
+#     '5-7,7-9',
+#     '2-8,3-7',
+#     '6-6,4-6',
+#     '2-6,4-8'
+# ]
+
+full_overlaps = 0
+for element in input_list:
+    range_input_str = [i.split('-') for i in element.split(',')]
+    range_input_int = [list(map(int, j)) for j in range_input_str]
+    x1, y1 = range_input_int[0]
+    x2, y2 = range_input_int[1]
+    if (x1 <= x2 and y1 >= y2) or \
+            (x2 <= x1 and y2 >= y1) or \
+            (x2 >= x1 and y1 >= x2) or \
+            (x2 <= x1 and y2 >= x1):
+        print(f'{range_input_int[0]}, {range_input_int[1]}')
+        full_overlaps+=1
+
+# answer
+full_overlaps
